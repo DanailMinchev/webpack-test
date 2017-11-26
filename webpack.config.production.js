4971cfbf4e68e68ab2747ed9136b97327b7cd39a
Danail Minchev <danail.minchev@gmail.com>
2017-11-26 15:45:00 +0000

    webpack.config.js: Sass configuration with separate stylesheet file using "extract-text-webpack-plugin"

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,7 +1,8 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: ['babel-polyfill', './src/app.js'],
+  entry: ['babel-polyfill', './src/js/app.js'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -16,7 +17,27 @@ module.exports = {
           loader: 'babel-loader',
           options: {}
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader' // translates CSS into CommonJS module
+            },
+            {
+              loader: 'sass-loader' // compiles Sass to CSS
+            }
+          ]
+        })
       }
     ]
-  }
+  },
+  plugins: [
+    // Usually, it's recommended to extract the style sheets into a dedicated file in production using the ExtractTextPlugin.
+    // This way your styles are not dependent on JavaScript.
+    new ExtractTextPlugin({
+      filename: 'main.css'
+    })
+  ]
 }
