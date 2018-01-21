37b20bcbe7ac48fcd498b818b81ae8d9f3f66576
Danail <DanailMinchev@users.noreply.github.com>
2018-01-21 12:23:24 +0000

    uglifyjs-webpack-plugin configuration

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,9 +1,15 @@
 // npm
 const webpackMerge = require('webpack-merge')
+const UglifyJSWebpackPlugin = require('uglifyjs-webpack-plugin')
 
 // local
 const webpackCommonConfig = require('./webpack.common.config')
 
 module.exports = webpackMerge(webpackCommonConfig, {
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSWebpackPlugin({
+      sourceMap: true
+    })
+  ]
 })
