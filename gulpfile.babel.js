dec3d2ba80423a04c7ca6b1a67f314b7221f7215
Danail <DanailMinchev@users.noreply.github.com>
2018-03-10 20:38:15 +0000

    "gulp -> webpack" migration

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,38 @@
 import gulp from 'gulp'
 import del from 'del'
+import webpack from 'webpack'
+import webpackDevelopmentConfig from './webpack.development.config'
+import webpackProductionConfig from './webpack.production.config'
+
+const NODE_ENV = process.env.NODE_ENV
+const IS_PRODUCTION = NODE_ENV === 'production'
 
 export const clean = () => {
   return del(['dist'])
 }
 
-gulp.task('default', defaultTask)
-
-function defaultTask (done) {
-  // place code for your default task here
-  console.log('processing defaultTask here ...')
-  done()
+export const buildWebpack = (done) => {
+  const webpackConfiguration =
+    IS_PRODUCTION ? webpackProductionConfig({NODE_ENV}) : webpackDevelopmentConfig({NODE_ENV})
+  webpack(webpackConfiguration, (err, stats) => {
+    if (err) {
+      console.error(err.stack || err)
+      if (err.details) {
+        console.error(err.details)
+      }
+      return
+    }
+    const info = stats.toJson()
+    if (stats.hasErrors()) {
+      console.error(info.errors)
+    }
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings)
+    }
+    done()
+  })
 }
+
+export const build = gulp.series(clean, gulp.parallel(buildWebpack))
+
+export default build
